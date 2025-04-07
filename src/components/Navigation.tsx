f866561b7be2983fa447b1ae6668b616de536440
Stefan Reeves <stefanreeves24@gmail.com>
2025-04-07 11:42:26 -0700

    fix nav logo

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -22,9 +22,10 @@ export default function Navigation() {
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo as an external link */}
         <a href={WEBSITE_URL} className="flex items-center">
-          <div className="text-3xl font-bold">
-            <span className="text-blue-600">Data</span>
-            <span className="text-gray-800">First</span>
+          <div>
+            <span className="text-gray-800 text-3xl font-bold">DataFirst</span>
+            <span className="text-sm"> </span>
+            <span className="text-blue-600 text-3xl">SEO</span>
           </div>
         </a>
       </div>
